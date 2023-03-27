9c99ef06f90ee528ce4c396f49659225b3d9a119
StefanGligorov <119980150+StefanGligorov@users.noreply.github.com>
2023-03-27 17:48:15 +0200

    Add files via upload

---
+++ Homeworks/SEDC.Class02.Homework/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// TASK 1
+//Console.WriteLine("Please enter 2 numbers");
+//string num1 = Console.ReadLine();
+//string num2 = Console.ReadLine();
+
+//int number1 = int.Parse(num1);
+//int number2 = int.Parse(num2);
+
+//int result = number1 + number2;
+
+//Console.WriteLine(result);
+
+// TASK 2
+//Console.WriteLine("Please enter 4 numbers");
+//string numb1 = Console.ReadLine();
+//string numb2 = Console.ReadLine();
+//string numb3 = Console.ReadLine(); 
+//string numb4 = Console.ReadLine();
+
+//int numberR1 = int.Parse(numb1);
+//int numberR2 = int.Parse(numb2);
+//int numberR3 = int.Parse(numb3);
+//int numberR4 = int.Parse(numb4);
+
+//int result1 = (numberR1 + numberR2 + numberR3 + numberR4) / 4;
+//Console.WriteLine(result1);
+
+// Task 3
+
+    int number1, number2, temp;
+    Console.Write("\nInput the First Number : ");
+    number1 = int.Parse(Console.ReadLine());
+    Console.Write("\nInput the Second Number : ");
+    number2 = int.Parse(Console.ReadLine());
+    temp = number1;
+    number1 = number2;
+    number2 = temp;
+    Console.Write("\nAfter Swapping : ");
+    Console.Write("\nFirst Number : " + number1);
+    Console.Write("\nSecond Number : " + number2);
+    Console.Read();
+
+
---
+++ Homeworks/SEDC.Class04.Homework/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Task 1
+Console.WriteLine("Please enter a word with more than 5 characters: ");
+
+string userInput = Console.ReadLine();
+string input = userInput.Substring(userInput.Length - 5);
+Console.WriteLine(input);
+Console.WriteLine("===========================================");
+// Task 2
+string userInput1 = "We are in Public Room and we enjoy some nice cold beer!";
+string[] input1 = userInput1.Split(" ");
+for (int i = 0; i < userInput1.Length; i++)
+{
+    Console.WriteLine(userInput1[i]);
+}
+Console.WriteLine("=========================================");
+
+//Task 3
+int SumOfTwoNumbers(int number1, int number2)
+{
+    int num1 = number1;
+    int num2 = number2;
+
+    return num1 + num2;
+}
+Console.WriteLine(SumOfTwoNumbers(20, 30));
+Console.WriteLine("=======================================");
+
+// Task 4
+Console.WriteLine("Enter date:YY ");
+string bDay = Console.ReadLine();
+int birthDay = int.Parse(bDay);
+
+int AgeCalculator(int birthDayDate)
+{ 
+    int age = birthDayDate;
+    return birthDay - age;
+}
+
+Console.WriteLine(AgeCalculator(1995));
